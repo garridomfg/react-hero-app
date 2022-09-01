2a46fab69b3e90296bb9afefb984ba626afcd524
garridomfg <garridomfg@gmail.com>
2022-09-01 10:39:28 +0200

    add route by params

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,5 +1,52 @@
+import { useMemo } from "react";
+import { Navigate, useParams, useNavigate } from "react-router-dom";
+import { getHeroById } from "../helpers";
+
 export const HeroPage = () => {
-  return <><h1>HeroPage</h1></>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const hero = useMemo(() => getHeroById(id), [id]);
+
+  const onNavigateBack = () => {
+    navigate(-1);
+  };
+
+  if (!hero) {
+    return <Navigate to="/marvel" />;
+  }
+
+  return (
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          className="img-thumbnail"
+          src={`/assets/heroes/${id}.jpg`}
+          alt={hero.superhero}
+        />
+      </div>
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego:</b> {hero.alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher:</b> {hero.publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First appearance:</b> {hero.first_appearance}
+          </li>
+        </ul>
+        <h5 className="mt-3">Characters</h5>
+        <p>{hero.characters}</p>
+
+        <button onClick={onNavigateBack} className="btn btn-outline-primary">
+          Back
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default HeroPage;
